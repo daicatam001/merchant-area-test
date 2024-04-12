fa6fcad471de27bf6c7f2126d315a8602a8448b9
tampt <phamthanhtam020295@gmail.com>
2024-04-12 10:11:40 +0700

    provide modal mode

--- sdk/onboarding/src/OnboardingSdk.ce.vue
+++ sdk/onboarding/src/OnboardingSdk.ce.vue
@@ -6,7 +6,8 @@ import onboardingSdkService from './service'
 
 </script>
 <template>
-  <div class="bg-red-400 h-screen flex flex-col">
+  <div class="bg-red-400 h-screen flex flex-col"
+    :class="{ 'fixed inset-0 w-full h-full z-100': onboardingSdkService.mode === 'modal' }">
     <h1 class="text-center">Onboarding SDK</h1>
     <div class="flex gap-6">
       <div>User Token:{{ onboardingSdkService.token }} </div>
